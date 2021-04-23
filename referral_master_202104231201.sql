d36da5dd77ff39a14d386d8b44aae6a10ee44ff3
Muhammad Kevin <muhammadkevinpf@gmail.com>
2021-04-23 12:04:29 +0700

    referral

---
+++ referral_master_202104231201.sql
@@ -0,0 +1,6 @@
+INSERT INTO pd_activity_management.referral_master (referral_code,referral_name,channel,rm_psid,branch_office_code,created_date,created_by,active_flag,updated_by,updated_date) VALUES
+	 ('54106101','VITRI HANDAYANI','UOB',NULL,NULL,'2021-04-23 09:08:17.852681+07','system',true,NULL,NULL),
+	 ('66036669','MOCHAMAD ERSJAD','UOB',NULL,NULL,'2021-04-23 09:08:17.852681+07','system',true,NULL,NULL),
+	 ('60551956','CHRISANTI ITA PRASETYA','UOB',NULL,NULL,'2021-04-23 09:08:17.852681+07','system',true,NULL,NULL),
+	 ('59661292','ALOYSIUS KEVIN','UOB',NULL,NULL,'2021-04-23 09:08:17.852681+07','system',true,NULL,NULL),
+	 ('54221469','SYARIFAH SARAH','UOB',NULL,NULL,'2021-04-23 09:08:17.852681+07','system',true,NULL,NULL);
