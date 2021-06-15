ab8b2f009200132371118eff3b41144b13a79597
Muhammad Kevin <43286448+muhammadkevinpf@users.noreply.github.com>
2021-06-15 17:53:21 +0700

    Add files via upload

---
+++ parameter_accounting_202106151743.sql
@@ -0,0 +1,30 @@
+INSERT INTO pd_pruladys.parameter_accounting (transaction_category,transaction_type,account_code,account_name,account_side,account_postkey,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,is_active) VALUES 
+('Accident Death Benefit','Reverse claim request (reject)','5111010000','Death','C','8','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Critical Illness','Claim Request','3141207000','Crisis Cover','D','9','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Critical Illness','Claim Request','3141990000','Other - Claim Reserve','C','9','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Critical Illness','Approve claim request','3141990000','Other - Claim Reserve','D','10','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Critical Illness','Approve claim request','3141207000','Crisis Cover','C','10','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Critical Illness','Payment to Customer','3141207000','Crisis Cover','D','11','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Critical Illness','Payment to Customer','1423008010','SCB Bank - 30601108970 Rp','C','11','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Critical Illness','Reverse claim request (reject)','3141990000','Other - Claim Reserve','D','12','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Critical Illness','Reverse claim request (reject)','5112070000','Crisis Cover','C','12','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Renewal Business','Receive Payment - Renewal Premium','1311211010','Premiums','C','4','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+;
+INSERT INTO pd_pruladys.parameter_accounting (transaction_category,transaction_type,account_code,account_name,account_side,account_postkey,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,is_active) VALUES 
+('New Business','Policy Issued','4112310010','Premiums','D','1','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('New Business','Policy Issued','1311211010','Premiums','C','1','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('New Business','Receipt new business premium','1423013010','UOBB Bank 3273003833 Rp','D','2','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('New Business','Receipt new business premium','1311211010','Premiums','C','2','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Renewal Business','Renewal premium recognition (Issued Billing)','1311211010','Premiums','D','3','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Renewal Business','Renewal premium recognition (Issued Billing)','4112310010','Premiums','C','3','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Renewal Business','Receive Payment - Renewal Premium','1423013010','UOBB Bank 3273003833 Rp','D','4','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Accident Death Benefit','Claim Request','5111010000','Death','D','5','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Accident Death Benefit','Claim Request','3141990000','Other - Claim Reserve','C','5','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Accident Death Benefit','Approve claim request','3141990000','Other - Claim Reserve','D','6','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+;
+INSERT INTO pd_pruladys.parameter_accounting (transaction_category,transaction_type,account_code,account_name,account_side,account_postkey,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,is_active) VALUES 
+('Accident Death Benefit','Approve claim request','3141101000','Death','C','6','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Accident Death Benefit','Payment to Customer','1423008010','Death','D','7','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Accident Death Benefit','Payment to Customer','1423008010','SCB Bank - 30601108970 Rp','C','7','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+,('Accident Death Benefit','Reverse claim request (reject)','3141990000','Other - Claim Reserve','D','8','ITNN000','LP1','206081','00','0000','0000','0000','P01','0000','00',true)
+;
