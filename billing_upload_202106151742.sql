5cf7a58aa3ff68bd8809d514adb9be527b994998
Muhammad Kevin <43286448+muhammadkevinpf@users.noreply.github.com>
2021-06-15 17:48:32 +0700

    Add files via upload

---
+++ billing_upload_202106151742.sql
@@ -0,0 +1,30 @@
+INSERT INTO pd_pruladys.billing_upload (id_periode,created_date,created_by,approved_date,approved_by,latest_file_flag,approval_status,id_policy,rejected_by,rejected_date) VALUES 
+(50,'2021-03-25 17:32:09.965','test','2021-03-25 17:32:23.125','',true,'Approve',1,NULL,NULL)
+,(59,'2021-04-09 15:41:30.682','test',NULL,NULL,true,'Pending',1,NULL,NULL)
+,(59,'2021-04-09 15:40:22.481','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(59,'2021-04-09 15:39:04.852','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(59,'2021-04-09 15:38:36.337','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(59,'2021-04-09 15:38:10.922','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(59,'2021-04-09 15:37:19.421','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(59,'2021-04-09 15:36:39.819','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(59,'2021-04-09 15:31:02.388','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(59,'2021-04-09 15:30:48.654','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+;
+INSERT INTO pd_pruladys.billing_upload (id_periode,created_date,created_by,approved_date,approved_by,latest_file_flag,approval_status,id_policy,rejected_by,rejected_date) VALUES 
+(59,'2021-04-09 15:28:55.981','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(59,'2021-04-09 15:26:24.667','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(59,'2021-04-09 15:25:55.143','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(59,'2021-04-09 15:24:55.871','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(59,'2021-04-09 15:24:19.021','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(59,'2021-04-09 15:23:32.358','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(51,'2021-05-20 15:34:56.356','test','2021-05-20 15:35:29.074','',true,'Approve',1,NULL,NULL)
+,(52,'2021-05-24 08:57:25.706','test','2021-05-24 08:57:53.785','',true,'Approve',1,NULL,NULL)
+,(54,'2021-06-15 08:18:43.663','test',NULL,NULL,false,'Rejected',1,'','2021-06-15 09:38:54.295')
+,(53,'2021-06-15 16:24:49.461','test',NULL,NULL,true,'Pending',1,NULL,NULL)
+;
+INSERT INTO pd_pruladys.billing_upload (id_periode,created_date,created_by,approved_date,approved_by,latest_file_flag,approval_status,id_policy,rejected_by,rejected_date) VALUES 
+(53,'2021-06-14 00:04:06.363','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(53,'2021-06-11 18:46:04.718','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(53,'2021-06-11 18:36:49.229','test',NULL,NULL,false,'Pending',1,NULL,NULL)
+,(53,'2021-06-14 00:11:56.262','test',NULL,NULL,false,'Rejected',1,'','2021-06-15 09:39:04.292')
+;
